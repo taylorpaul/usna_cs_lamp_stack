8720015991189a12f6d51ab7d80b28858c58c790
Taylor Paul <tpaul@usna.edu>
2020-03-16 14:05:57 -0400

    Removing insideous hidden unicode spaces, deleting comments from stuff yet completed

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # TODO: Update mAlpha Below:
-​MALPHA="mXXXXXX"
+MALPHA="mXXXXXX"
 #
 # Script Globals/Software Versions (students should NOT update):
 MYSQL_VERSION=5.7
@@ -30,142 +30,10 @@ sudo sed -i 's_IncludesNoExec_ExecCGI\n\t\tAddhandler cgi-script .py .cgi_g' /et
 sudo sed -i 's=php_admin_flag engine Off=#php_admin_flag engine Off=g' /etc/apache2/mods-enabled/php$PHP_VERSION.conf
 sudo sed -i 's|post_max_size = 8M|post_max_size = 24M|g' /etc/php/$PHP_VERSION/apache2/php.ini
 sudo sed -i 's|upload_max_filesize = 2M|upload_max_filesize = 24M|g' /etc/php/$PHP_VERSION/apache2/php.ini
-sudo sed -i 's|Options Indexes FollowSymLinks|Options Indexes FollowSymLinks ExecCGI|g' /etc/apache2/apache2.conf
+#
 #NOTE: for compatability with non-GNU version of sed, save output to a tmp file and then mv to original file
 sudo sed -e '/Options Indexes FollowSymLinks/{s//Options Indexes FollowSymLinks ExecCGI\n\tAddhandler cgi-script .py .cgi/;:a' -e '$!N;$!ba' -e '}' /etc/apache2/apache2.conf > apache2.conf.tmp
 sudo mv apache2.conf.tmp /etc/apache2/apache2.conf
-sudo rm apache2.conf.tmp
 # If/When prompted enter the machine sudo password
-​#
-#
-​
+
 # # TODO: Stopped HERE!!!
-#
-# # 2) CHANGE DOCUMENT ROOT TO PUBLIC_HTML DIR:
-# ​
-# # Changing apache2 document root
-# ​
-# # The default document root is set in the 000-default.conf file that is under /etc/apache2/sites-available folder.
-# ​
-# $ cd /etc/apache2/sites-available
-# ​
-# $ sudo nano 000-default.conf
-# ​
-# # While the file is opened change DocumentRoot /var/www/ with your new folder e.g DocumentRoot /home/shprink/Sites/
-# ​
-# # Set the right Apache configuration
-# ​
-# T# he configuration of the /etc/www folder is under /etc/apache2/apache2.conf. Edit this file to add the configuration of your new document root.
-# ​
-# $ sudo nano /etc/apache2/apache2.conf
-# ​
-# # Copy the following:
-# ​
-# <Directory /var/www/>
-# ​
-#        Options Indexes FollowSymLinks
-# ​
-#        AllowOverride None
-# ​
-#        Require all granted
-# ​
-# </Directory>
-# ​
-# # and change the directory path:
-# ​
-# <Directory /home/<YOUR_USER_NAME>/public_html> # MAKE THIS MATCH your pathto ~/public_html!!
-# ​
-#        Options Indexes FollowSymLinks
-# ​
-#        AllowOverride None
-# ​
-#        Require all granted
-# ​
-# </Directory>
-# ​
-# Restart Apache
-# ​
-# $ sudo service apache2 restart
-# ​
-# # Set the right permission
-# ​
-# # All of your document root parent folders must be executable by everyone. To know if it is the case you can use the utility namei that list the permissions along each component of the path:
-# ​
-# $ namei -m /home/shprink/Sites/
-# ​
-# # f: /home/shprink/Sites/
-# ​
-#  # drwxr-xr-x /
-# ​
-#  # drwxr-xr-x home
-# ​
-# #  drwx------ shprink
-# ​
-#  # drwx------ Sites
-# ​
-# # Here as you can see that shprink and Sites permissions are not set properly.
-# ​
-# # Open http://localhost/ in your browser, you should get the following message:
-# ​
-# # Forbidden
-# ​
-# # You don’t have permission to access / on this server.
-# ​
-# # Open the apache error log to see the exact error code e.g AH00035. It might help you to get more information.
-# ​
-# $ sudo tail -f /var/log/apache2/error.log
-# ​
-# # [Mon Apr 06 09:04:26.518260 2015] [core:error] [pid 22139] (13)Permission denied: [client 127.0.0.1:45121] AH00035: access to / denied (filesystem path '/home/shprink/Sites') because search permissions are missing on a component of the path
-# ​
-# # To fix the permission problem for good, using chmod +755 should be enough.
-# ​
-# $ chmod +755 /home/shprink/
-# ​
-# $ chmod +755 /home/shprink/Sites/
-# ​
-# # Re run namei to make sure everything is ok.
-# ​
-# $ namei -m ~/Sites/
-# ​
-# # f: /home/shprink/Sites/
-# ​
-#  # drwxr-xr-x /
-# ​
-#  # drwxr-xr-x home
-# ​
-#  # drwxr-xr-x shprink
-# ​
-# # drwxr-xr-x Sites
-# ​
-# ​
-# # Change Port number, ITSD blocks hosts from using port 80:
-# sudo vim /etc/apache2/ports.conf
-# ​
-# # Change the port number to a port of your choice, recommend 81:
-# Listen 80 -> Listen 81
-# ​
-# # Restart and test apache:
-# sudo service apache2 restart
-# ​
-# # Now opening http://localhost:81/ should work as expected.
-# ​
-# ​
-# # Start MySQL service:
-# ​
-# Sudo service mysql restart
-# ​
-# # Create username:
-# GRANT ALL PRIVILEGES ON *.* TO 'mALPHA'@'localhost' IDENTIFIED BY 'password';
-# ​
-# # Login as username:
-# ​
-# # Type exit and then relogin from WSL:
-# mysql -u mALPHA -p                 # (Notice no -h here, we are connecting to mysql running on our localhost!)
-# ​
-# # CREATE a Database:
-# ​
-# CREATE DATABASE mALPHA;
-# ​
-# USE mALPHA;
-# ​
-# # NOTE: In your mysql.inc.php file, have a login set for localhost and if you use the same uname, schema and password for your local mysql copy, you will connect to the right instance depending on where you are running your code!
